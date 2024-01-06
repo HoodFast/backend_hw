b14a4e4bc2fa7aa0fe2e1d1a40ac1e735412ff86
HoodFast <rabota-trassa@mail.ru>
2024-01-07 02:56:29 +0300

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const settings_1 = require("./settings");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 settings_1.app.listen(port, () => {
     console.log(`App start on port ${port}`);
 });
