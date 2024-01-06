0863bcb5cc8a5bfec5be6184ec62411582ae20d0
HoodFast <rabota-trassa@mail.ru>
2024-01-06 21:33:37 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {app} from "./settings";
 
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 
 app.listen(port,()=>{
     console.log(`App start on port ${port}`)
